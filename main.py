fd3287aeba2f8297b17efcfaf8062f6f3e3fa0a9
nadamhmd <nadamt3@gmail.com>
2021-10-25 10:40:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+import pandas as pd 
+import numpy as np
+
+
+from sklearn.metrics import mean_squared_error, r2_score
+from multiprocessing import Pool 
+from sklearn.externals import joblib
+from joblib import parallel_backend
+from joblib import Parallel, delayed
+
+
+
+
+
+
+if __name__ == "__main__":
+  Continue
